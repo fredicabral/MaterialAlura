a8c7453076801f9dc500b3cbc10696567544a612
fredicabral <fredicabral@hotmail.com>
2022-06-04 16:21:33 -0300

    Material JS Alura

---
+++ type-boolean.js
@@ -0,0 +1,12 @@
+//Boolean
+//true - > Verdadeiro
+//false - > Falso
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+ 
+const texto1 = "Fred"
+const texto2 = "fred";
+
+//console.log(primeiroNumero === segundoNumero);
+console.log(texto1 === texto2);
---
+++ type-string.js
@@ -0,0 +1,15 @@
+const texto1 = "Olá mundo, é o Frederico!";
+const texto2 = 'Olá mundo, é o Frederico'
+const stringDeNumeros = '7 x 1 Brasil e Alemanhã';
+
+
+const citacao = "Meu nome é ";
+const nome = "frederico";
+
+const input = nome.toLowerCase();
+
+const senha = "o brasil é verde e amarelo";
+
+console.log(nome == input);
+
+console.log(senha.length);
