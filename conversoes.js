aacc844dfc6688e5fd9de3eaf67acae162a59c0e
fredicabral <fredicabral@hotmail.com>
2022-06-07 22:53:59 -0300

    Var e Conversões

---
+++ conversoes.js
@@ -0,0 +1,18 @@
+// tipo de dado
+// booleanos
+
+// conversao implicita
+const numero = 456;
+const numeroString = Number("456");
+
+
+//Number()
+//String()
+
+//console.log(numero == numeroString);
+console.log(numero + Number(numeroString));
+console.log(numero + numeroString);
+
+
+// conversao explicita
+
---
+++ truthy-falsy.js
@@ -0,0 +1,28 @@
+// // boolean
+
+// const usuarioLogado = true;
+// const contaPaga = false;
+
+// // truthy ou falsy
+
+// // 0 => true
+// // 1 => false
+
+// console.log(0 == false);
+// console.log("" == false);
+// console.log(1 == true);
+
+//null
+//undefined
+
+let minhaVar;
+let varNull = null;
+
+let numero = 3;
+let texto  = "Alura";
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
+
+
+
